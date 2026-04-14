3b7169fc8c7de99b667414bf00eda7becccea010
Aaron Anu <231284258+aaron-anu@users.noreply.github.com>
2026-04-14 13:11:53 +0530

    Second Commit

---
+++ src/lib/tasks.py
@@ -0,0 +1,18 @@
+import csv
+import os 
+import time
+
+class Task:
+    def __init__(self, no, task_file):
+        self.no=no
+        # populate task_name, description, due_time, birth_time
+        task_name=""
+        description=""
+        due_time=""
+        birth_time=""
+    
+    def save(self):
+        """Save task to file"""
+        pass
+
+
