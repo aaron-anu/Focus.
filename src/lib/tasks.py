24b1cb8144b02eb139239ad46ac946c2722b2e54
Aaron Anu <231284258+aaron-anu@users.noreply.github.com>
2026-04-15 14:46:26 +0530

    third commit

--- src/lib/tasks.py
+++ src/lib/tasks.py
@@ -1,18 +1,17 @@
-import csv
-import os 
-import time
+# import csv
+# import os
+# import time
+
 
 class Task:
     def __init__(self, no, task_file):
-        self.no=no
+        self.no = no
         # populate task_name, description, due_time, birth_time
-        task_name=""
-        description=""
-        due_time=""
-        birth_time=""
-    
+        task_name = ""
+        description = ""
+        due_time = ""
+        birth_time = ""
+
     def save(self):
         """Save task to file"""
         pass
-
-
