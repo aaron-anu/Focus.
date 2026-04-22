7d1f663a138b89dc0d6e780cee09fc2be5f15967
Aaron Anu <231284258+aaron-anu@users.noreply.github.com>
2026-04-22 22:36:25 +0530

    Running logic

--- src/main-tui.py
+++ src/main-tui.py
@@ -1,13 +1,30 @@
+import os
+from profile import run
+
 import lib.tasks as ts
 
 
 def main():
-    pass
+    print("Initializing")
+
+    # create data dir
+    data_dir = os.path.dirname(os.path.abspath(__file__)) + "/../data"
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    running = True
+    while running:
+        print("""1. Add tasks
+2. List tasks
+3. Mark as done
+4. Start timer
+*. Exit""")
+        choice = input("Enter choice: ")
+        match choice:
+            case _:
+                running = False
+                print("Exiting...")
 
 
-new = ts.Task()
-
-running = True
-while running:
+if __name__ == "__main__":
     main()
-    running = bool(input())
