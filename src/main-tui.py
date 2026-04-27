e257d49a6cb55c34654f3fd4ac6c88cba41beaf4
krizxjr <abhedhkrishnanjr@gmail.com>
2026-04-27 22:54:52 +0530

    Add files via upload

--- src/main-tui.py
+++ src/main-tui.py
@@ -1,7 +1,14 @@
 import os
-from profile import run
+from datetime import datetime
+from keyword import iskeyword
+
+import pandas as pd
 
 import lib.tasks as ts
+
+df = pd.read_csv("")
+
+# (id, title, birthtime, duetime, done, desc)
 
 
 def main():
@@ -20,7 +27,27 @@ def main():
 4. Start timer
 *. Exit""")
         choice = input("Enter choice: ")
+
         match choice:
+            case "1":
+                tit = input("Enter title of task: ")
+                desc = input("Enter desc of task: ")
+                due = input("Enter duetime of task: ")
+                birthtime = datetime.now()
+                done = False
+
+            case "2":
+                print("aaaaaa")
+            case "3":
+                tit = input("Enter the title of task completed: ")
+                if (df["title"] == tit).any():
+                    df.loc[df["title"] == tit, df.columns[4]] = True
+                    print("Task marked as completed")
+                else:
+                    print("No matching title found")
+
+            case 4:
+                print("aaaaaa")
             case _:
                 running = False
                 print("Exiting...")
